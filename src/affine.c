396288757b52eeec17a3724bfa90318579322a26
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-10-27 13:10:46 -0700

    add affine

--- src/affine.c
+++ src/affine.c
@@ -11,7 +11,7 @@
 #include "raspberry_128x128_bgar5515.h"
 
 #define SPRITE_CONFIG 0xF000
-#define SPRITE_LENGTH 16
+#define SPRITE_LENGTH 6
 
 void main()
 {
@@ -27,6 +27,11 @@ void main()
         int xv;
         int yv;
     } vectors[SPRITE_LENGTH];
+    struct
+    {
+        int v;
+        int d;
+    } scale[SPRITE_LENGTH];
 
     // Copy sprite data
     RIA.addr0 = 0;
@@ -37,7 +42,8 @@ void main()
     // Initial sprite positions
     for (u = 0; u < SPRITE_LENGTH; u++)
     {
-        unsigned ptr = SPRITE_CONFIG + u * sizeof(vga_mode4_sprite_t);
+
+        unsigned ptr = SPRITE_CONFIG + u * sizeof(vga_mode4_asprite_t);
         unsigned x = ((uint32_t)rand() * (320 - 128)) >> 15;
         unsigned y = ((uint32_t)rand() * (240 - 128)) >> 15;
         sprites[u].x = x;
@@ -50,17 +56,28 @@ void main()
             vectors[u].yv = 1;
         else
             vectors[u].yv = -1;
-        xram0_struct_set(ptr, vga_mode4_sprite_t, x_pos_px, x);
-        xram0_struct_set(ptr, vga_mode4_sprite_t, y_pos_px, y);
-        xram0_struct_set(ptr, vga_mode4_sprite_t, xram_sprite_ptr, 0x0000);
-        xram0_struct_set(ptr, vga_mode4_sprite_t, log_size, 7);
-        xram0_struct_set(ptr, vga_mode4_sprite_t, has_opacity_metadata, true);
+        scale[u].v = (((uint32_t)rand() * 512) >> 15) + 256;
+        if (((uint32_t)rand() * 2) >> 15)
+            scale[u].d = 3;
+        else
+            scale[u].d = -3;
+        xram0_struct_set(ptr, vga_mode4_asprite_t, transform[0], scale[u].v);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, transform[1], 0);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, transform[2], 0);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, transform[3], 0);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, transform[4], scale[u].v);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, transform[5], 0);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, x_pos_px, x);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, y_pos_px, y);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, xram_sprite_ptr, 0x0000);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, log_size, 7);
+        xram0_struct_set(ptr, vga_mode4_asprite_t, has_opacity_metadata, true);
     }
 
     // Program VGA
     xreg_vga_canvas(1);
     xreg_vga_mode(0);
-    xreg_vga_mode(4, 0, SPRITE_CONFIG, SPRITE_LENGTH);
+    xreg_vga_mode(4, 1, SPRITE_CONFIG, SPRITE_LENGTH);
 
     // Vsync loop
     v = RIA.vsync;
@@ -71,23 +88,34 @@ void main()
         v = RIA.vsync;
 
         // Copy positions during vblank
-        RIA.addr0 = SPRITE_CONFIG;
-        RIA.step0 = sizeof(vga_mode4_sprite_t);
-        RIA.addr1 = SPRITE_CONFIG + 1;
-        RIA.step1 = sizeof(vga_mode4_sprite_t);
+        RIA.addr0 = SPRITE_CONFIG + 12;
+        RIA.step0 = sizeof(vga_mode4_asprite_t);
+        RIA.addr1 = SPRITE_CONFIG + 13;
+        RIA.step1 = sizeof(vga_mode4_asprite_t);
         for (u = 0; u < SPRITE_LENGTH; u++)
         {
             int val = sprites[u].x;
             RIA.rw0 = val & 0xff;
             RIA.rw1 = (val >> 8) & 0xff;
         }
-        RIA.addr0 = SPRITE_CONFIG + 2;
-        RIA.addr1 = SPRITE_CONFIG + 3;
+        RIA.addr0 = SPRITE_CONFIG + 14;
+        RIA.addr1 = SPRITE_CONFIG + 15;
         for (u = 0; u < SPRITE_LENGTH; u++)
         {
             int val = sprites[u].y;
             RIA.rw0 = val & 0xff;
             RIA.rw1 = (val >> 8) & 0xff;
+        }
+
+        // explode
+        for (u = 0; u < SPRITE_LENGTH; u++)
+        {
+            unsigned ptr = SPRITE_CONFIG + u * sizeof(vga_mode4_asprite_t);
+            scale[u].v += scale[u].d;
+            if (scale[u].v < 256 || scale[u].v > 768)
+                scale[u].d *= -1;
+            xram0_struct_set(ptr, vga_mode4_asprite_t, transform[0], scale[u].v);
+            xram0_struct_set(ptr, vga_mode4_asprite_t, transform[4], scale[u].v);
         }
 
         // Update positions
