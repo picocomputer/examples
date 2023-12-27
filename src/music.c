1e07f7060ae60d470478b44586bf38b608ff728b
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2023-12-27 11:01:06 -0800

    pre adsr

--- src/music.c
+++ src/music.c
@@ -8,67 +8,66 @@
 #include <rp6502.h>
 #include <stdint.h>
 
-#define  c2 65
+#define c2 65
 #define cs2 69
-#define  d2 73
+#define d2 73
 #define ds2 78
-#define  e2 82
-#define  f2 87
+#define e2 82
+#define f2 87
 #define fs2 92
-#define  g2 98
+#define g2 98
 #define gs2 104
-#define  a2 110
+#define a2 110
 #define as2 117
-#define  b2 123
+#define b2 123
 
-#define  c3 131
+#define c3 131
 #define cs3 139
-#define  d3 147
+#define d3 147
 #define ds3 156
-#define  e3 165
-#define  f3 175
+#define e3 165
+#define f3 175
 #define fs3 185
-#define  g3 196
+#define g3 196
 #define gs3 208
-#define  a3 220
+#define a3 220
 #define as3 233
-#define  b3 247
+#define b3 247
 
-#define  c4 262
+#define c4 262
 #define cs4 277
-#define  d4 294
+#define d4 294
 #define ds4 311
-#define  e4 330
-#define  f4 349
+#define e4 330
+#define f4 349
 #define fs4 370
-#define  g4 392
+#define g4 392
 #define gs4 415
-#define  a4 440
+#define a4 440
 #define as4 466
-#define  b4 494
+#define b4 494
 
-#define  c5 523
+#define c5 523
 #define cs5 554
-#define  d5 587
+#define d5 587
 #define ds5 622
-#define  e5 659
-#define  f5 698
+#define e5 659
+#define f5 698
 #define fs5 740
-#define  g5 784
+#define g5 784
 #define gs5 831
-#define  a5 880
+#define a5 880
 #define as5 932
-#define  b5 988
-
+#define b5 988
 
 struct channels
 {
-    uint16_t freq;
     uint16_t duty;
-    uint8_t trig_wave;
-    uint8_t attack_vol;
-    uint8_t decay_vol;
-    uint8_t release_pan;
+    uint16_t freq;
+    uint8_t pan_trig;
+    uint8_t vol_attack;
+    uint8_t vol_decay;
+    uint8_t wave_release;
 };
 
 void sleep(unsigned n)
@@ -79,29 +78,30 @@ void sleep(unsigned n)
             ;
 }
 
-play(uint16_t f)
+void play(uint16_t f)
 {
     xram0_struct_set(0xFF00, struct channels, freq, f);
-    xram0_struct_set(0xFF00, struct channels, trig_wave, 0x12);
+    xram0_struct_set(0xFF00, struct channels, pan_trig, 0x01);
 }
 
-stop()
+void stop()
 {
-    xram0_struct_set(0xFF00, struct channels, trig_wave, 0x00);
+    xram0_struct_set(0xFF00, struct channels, pan_trig, 0x00);
 }
 
 void main(void)
 {
     // xreg(0, 1, 0xFF, 0xFF00);
 
+    xram0_struct_set(0xFF00, struct channels, vol_attack, 0x00);
+    xram0_struct_set(0xFF00, struct channels, vol_decay, 0x00);
+    xram0_struct_set(0xFF00, struct channels, wave_release, 0x00);
+    xram0_struct_set(0xFF00, struct channels, pan_trig, 0x00);
     xram0_struct_set(0xFF00, struct channels, freq, 50);
-    xram0_struct_set(0xFF00, struct channels, duty, 65535);
-    // xram0_struct_set(0xFF00, struct channels, duty, 32768);
+    // xram0_struct_set(0xFF00, struct channels, duty, 65535);
+    xram0_struct_set(0xFF00, struct channels, duty, 32768);
     // xram0_struct_set(0xFF00, struct channels, duty, 5000);
-    xram0_struct_set(0xFF00, struct channels, trig_wave, 0x04);
-    xram0_struct_set(0xFF00, struct channels, attack_vol, 0x00);
-    xram0_struct_set(0xFF00, struct channels, decay_vol, 0x00);
-    xram0_struct_set(0xFF00, struct channels, release_pan, 0x00);
+
     xreg(0, 1, 0x00, 0xFF00);
     sleep(1);
 
@@ -171,5 +171,4 @@ void main(void)
 
     play(a4);
     sleep(2);
-
 }
