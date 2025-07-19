1b5aa5eedf1870d194d7d83c12b325922b74f281
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2025-07-19 00:27:20 -0700

    Gamepad update (#8)

--- src/gamepad.c
+++ src/gamepad.c
@@ -8,77 +8,98 @@
 #include <rp6502.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #define xreg_ria_gamepad(...) xreg(0, 0, 2, __VA_ARGS__)
 
-void show()
+void print(bool enabled, const char *str)
 {
-    const char *dpad[] = {"N ", "NE ", "E ", "SE ", "S ", "SW ", "W ", "NW ", ""};
-    uint8_t btns1, btns2, btns3;
-
-    printf("lx:%3u ly:%3u rx:%3u ry:%3u ", RIA.rw0, RIA.rw0, RIA.rw0, RIA.rw0);
-    btns1 = RIA.rw0;
-    btns2 = RIA.rw0;
-    btns3 = RIA.rw0;
-    printf("l2:%3u r2:%3u ", RIA.rw0, RIA.rw0);
-
-    if (btns1 & 0x0F == 0xF)
+    if (enabled)
     {
-        printf("Disconnected\n");
+        // Green background with black text for better visibility
+        printf("\033[30;42m%s\033[0m ", str);
+    }
+    else
+    {
+        // Print bright black text (dark gray) instead of dim
+        printf("\033[90m%s\033[0m ", str);
+    }
+}
+
+void show(int player)
+{
+    const char *dpad[] = {"0 ", "N ", "S ", "ER",
+                          "W ", "NW", "SW", "ER",
+                          "E ", "NE", "SE", "ER",
+                          "ER", "ER", "ER", "ER"};
+    uint8_t hat, sticks, btns0, btns1;
+
+    printf("P%d ", player);
+
+    hat = RIA.rw0;
+    sticks = RIA.rw0;
+    btns0 = RIA.rw0;
+    btns1 = RIA.rw0;
+
+    printf("lx:%4d ly:%4d ", (int8_t)RIA.rw0, (int8_t)RIA.rw0);
+    printf("rx:%4d ry:%4d ", (int8_t)RIA.rw0, (int8_t)RIA.rw0);
+    printf("lt:%3u rt:%3u ", RIA.rw0, RIA.rw0);
+    printf("L:%s ", dpad[sticks & 0xF]);
+    printf("R:%s ", dpad[(sticks & 0xF0) >> 4]);
+    printf("H:%s ", dpad[hat & 0xF]);
+
+    if (!(hat & 0x80))
+    {
+        printf("\33[K\n\033[90m   Disconnected\033[0m\33[K\n\n");
         return;
     }
 
-    printf("%s", dpad[btns1 & 0xf]);
-    if (btns1 & 0x10)
-        printf("Square ");
-    if (btns1 & 0x20)
-        printf("Cross ");
-    if (btns1 & 0x40)
-        printf("Circle ");
-    if (btns1 & 0x80)
-        printf("Triangle ");
+    printf("\n   ");
 
-    if (btns2 & 0x01)
-        printf("L1 ");
-    if (btns2 & 0x02)
-        printf("R1 ");
-    if (btns2 & 0x04)
-        printf("L2 ");
-    if (btns2 & 0x08)
-        printf("R2 ");
-    if (btns2 & 0x10)
-        printf("Share ");
-    if (btns2 & 0x20)
-        printf("Option ");
-    if (btns2 & 0x40)
-        printf("L3 ");
-    if (btns2 & 0x80)
-        printf("R3 ");
+    if (hat & 0x40)
+    {
+        print(btns0 & 0x01, "Cross");
+        print(btns0 & 0x02, "Circle");
+        print(btns0 & 0x04, "Square");
+        print(btns0 & 0x08, "Triangle");
+    }
+    else
+    {
+        print(btns0 & 0x01, "A");
+        print(btns0 & 0x02, "B");
+        print(btns0 & 0x04, "X");
+        print(btns0 & 0x08, "Y");
+    }
 
-    if (btns3 & 0x01)
-        printf("PS ");
-    if (btns3 & 0x02)
-        printf("Tpad ");
+    print(btns0 & 0x10, "L1");
+    print(btns0 & 0x20, "R1");
+    print(btns0 & 0x40, "Select");
+    print(btns0 & 0x80, "Start");
 
-    printf("\n");
+    print(btns1 & 0x01, "L2");
+    print(btns1 & 0x02, "R2");
+    print(btns1 & 0x04, "L3");
+    print(btns1 & 0x08, "R3");
+    print(btns1 & 0x10, "Home");
+
+    // Unknown buttons
+    if (btns1 & 0xE0)
+        printf("\033[90m0x%02X\033[0m", btns1 & 0xE0);
+    printf("\33[K\n\n");
 }
 
 void main()
 {
-    unsigned u;
+    printf("\30\33c\nPicocomputer 6502 Gamepad Tester");
     xreg_ria_gamepad(0xFF00);
-
     while (1)
     {
-        while (--u)
-            ;
-        // Read all the values. Note that you can ignore the analog inputs
-        // and any button set you don't care about.
+        printf("\33[H\33[3B");
         RIA.addr0 = 0xFF00;
         RIA.step0 = 1;
-        printf("\nP1 ");
-        show();
-        printf("P2 ");
-        show();
+        show(1);
+        show(2);
+        show(3);
+        show(4);
     }
 }
