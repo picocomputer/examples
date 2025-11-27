a157c0a084439b73de3ce0d6cd0eb3fea995544d
rumbledethumps <16963588+rumbledethumps@users.noreply.github.com>
2025-11-27 14:14:02 -0800

    prob matcher

--- tools/cc65_wrapper.cmake
+++ tools/cc65_wrapper.cmake
@@ -1,18 +1,18 @@
 # Wrapper script to filter out arguments that cc65 doesn't support.
 # This enables passing extra arguments to IntelliSense.
 
-# Args 0-3 are the cmake call to this script
+# Args 0-3 are the cmake call to this script.
 if(NOT CMAKE_ARGV3 STREQUAL "--")
     message(FATAL_ERROR "No -- separator found in arguments")
 endif()
 
-# First argument after -- is CC65_COMPILER
+# First argument after -- is CC65_COMPILER.
 set(CC65_COMPILER "${CMAKE_ARGV4}")
 if(NOT EXISTS "${CC65_COMPILER}")
     message(FATAL_ERROR "Could not find executable: ${CC65_COMPILER}")
 endif()
 
-# Remove -include and its argument
+# Remove -include and its argument.
 set(FILTERED_ARGS "")
 set(SKIP_NEXT FALSE)
 foreach(INDEX RANGE 5 ${CMAKE_ARGC})
@@ -28,13 +28,27 @@ foreach(INDEX RANGE 5 ${CMAKE_ARGC})
     endif()
 endforeach()
 
-# Execute the real compiler with filtered arguments
+# Execute the real compiler with filtered arguments.
 execute_process(
     COMMAND ${CC65_COMPILER} ${FILTERED_ARGS}
-    RESULT_VARIABLE RESULT
+    OUTPUT_VARIABLE STDOUT_OUTPUT
+    ERROR_VARIABLE STDERR_OUTPUT
+    RESULT_VARIABLE EXIT_CODE
 )
 
-# Propagate the exit code without any CMake error message
-if(NOT RESULT EQUAL 0)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E false)
+# Output stdout unchanged.
+if(STDOUT_OUTPUT)
+    message(STATUS "${STDOUT_OUTPUT}")
+endif()
+
+# Reformat stderr so VS Code problem matcher works. Yes, it's just the case.
+if(STDERR_OUTPUT)
+    string(REGEX REPLACE "(:[0-9]+:) Error:" "\\1 error:" STDERR_OUTPUT "${STDERR_OUTPUT}")
+    string(REGEX REPLACE "(:[0-9]+:) Warning:" "\\1 warning:" STDERR_OUTPUT "${STDERR_OUTPUT}")
+    message(NOTICE "${STDERR_OUTPUT}")
+endif()
+
+# Return a non-zero exit code.
+if(NOT EXIT_CODE EQUAL 0)
+    message(FATAL_ERROR "Compilation failed with exit code ${EXIT_CODE}")
 endif()
